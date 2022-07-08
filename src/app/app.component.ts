76632cb8faf6d254f7ff2e0227a747a6188c2cf7
asal <asal13adib78@gmail.com>
2022-07-08 14:23:24 +0430

    init main structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cookingSample';
+
+  infoBoxes = [
+    {
+      Title: 'Heavenly Halibut',
+      Desc: 'When it comes to seafood, halibut is hard to beat. This delicious white fish is not only prized for its delicate flavor but also its versatility. Halibut can be baked, grilled, mashed, or even used in a hearty stew.'
+    },
+    {
+      Title: 'Baked Haddock',
+      Desc: 'Baked haddock is a delicious, healthy seafood dish perfect for a weeknight meal. The fish cover in a light breadcrumb and herb crust, then baked until flaky and tender.'
+    },
+    {
+      Title: 'Easy Tuna Casserole',
+      Desc: 'Who says easy and delicious cant go hand-in-hand? This easy tuna casserole recipe proves you can have your cake and eat it too.'
+    },
+    {
+      Title: 'Scrumptious Salmon Cakes',
+      Desc: 'As any seafood lover knows, few things are as delicious as a salmon cake. These bite-sized morsels are with flavor, and their crispy exterior perfectly contrasts the moist and flaky fish within.'
+    } 
+  ]
+
+
+
 }
---
+++ src/app/info-box/info-box.component.ts
@@ -0,0 +1,18 @@
+import { Input , Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-box',
+  templateUrl: './info-box.component.html',
+  styleUrls: ['./info-box.component.css']
+})
+export class InfoBoxComponent implements OnInit {
+
+  @Input() Title!: string;
+  @Input() Desc!: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
